5732b7290337078b042e989d4e7f05c0e881d8ab
NicolaRebecca <master9895@gmail.com>
2021-06-04 09:50:16 +0200

    pulito un pò il codice

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-var cognomiArray = ['bianchi','rossi','verdi','fumagalli','brambilla','scotti']
+var cognomiArray = ['bianchi','rossi','verdi','fumagalli','brambilla','scotti'];
+var cognomeUtente = prompt("inserisci il cognome");
+ cognomiArray.push(cognomeUtente);
+ 
+ 
 
-
-while ( cognomeUtente = prompt("inserisci il cognome")){
- cognomiArray.push(cognomeUtente)
- break
-}
 
 for( var i = 0; i < cognomiArray.length; i++){
     cognomiArray.sort()
